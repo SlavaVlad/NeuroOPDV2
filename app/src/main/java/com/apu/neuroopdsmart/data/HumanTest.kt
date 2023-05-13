f32418b6895a242c6d4fa08d76b7cc9c6902ed21
Vladislav <nauka.2.0.vs@gmail.com>
2023-05-13 15:59:30 +0300

    начал писать интеграцию первого теста

--- app/src/main/java/com/apu/neuroopdsmart/data/HumanTest.kt
+++ app/src/main/java/com/apu/neuroopdsmart/data/HumanTest.kt
@@ -1,5 +1,6 @@
 package com.apu.neuroopdsmart.data
 
+import android.util.Log
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Icon
@@ -12,6 +13,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.lifecycle.MutableLiveData
 import com.apu.neuroopdsmart.R
+import com.apu.neuroopdsmart.maketag
 import java.time.Duration
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -116,10 +118,12 @@ open class HumanTest(
 
     override fun stateOnSuccess(attempts: Int, result: Float) {
         onSuccess(attempts, result)
+        Log.i(maketag(this), "stateOnSuccess: ")
     }
 
     override fun stateOnFailed() {
         onFailed()
+
     }
 
     open override fun TestContainer(onTriggered: (result: Float, time: Long) -> Unit) {
